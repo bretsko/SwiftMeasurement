628f1a6222f59564aec93ab2f74ad3e86089514c
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2019-08-23 19:06:15 +0300

    update

--- Package.swift
+++ Package.swift
@@ -1,5 +1,33 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftMeasurement"
+    name: "MeasureKit",
+    
+    platforms: [
+        .iOS(.v10),
+    ],
+    
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "MeasureKit",
+            targets: ["MeasureKit"]),
+    ],
+    dependencies: [
+        //
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "MeasureKit",
+            dependencies: []),
+        
+        .testTarget(
+            name: "MeasureKitTests",
+            dependencies: ["MeasureKit"]),
+    ]
 )
